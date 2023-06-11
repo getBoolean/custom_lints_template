c073e556001294c7ca08d6068ab71f20c5a8aee3
getBoolean <getboolean@gmail.com>
2023-06-11 02:50:07 -0500

    rename private class

--- lib/custom_lints_template.dart
+++ lib/custom_lints_template.dart
@@ -1,9 +1,9 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:custom_lints_template/src/lints/avoid_hardcoded_strings.dart';
 
-PluginBase createPlugin() => _BooleanPlugin();
+PluginBase createPlugin() => _CustomLintTemplatePlugin();
 
-class _BooleanPlugin extends PluginBase {
+class _CustomLintTemplatePlugin extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
         AvoidHardcodedStringsRule(),
