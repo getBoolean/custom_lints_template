1e2f24d4c9685aa0834afccc3351a4d66f2fef7e
getBoolean <danielwnewville@gmail.com>
2023-10-16 21:52:09 -0500

    Fix windows not finding file

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -2,7 +2,7 @@ import 'package:example/example.dart' as example;
 
 void main(List<String> arguments) {
   // expect_lint: avoid_hardcoded_strings
-  const test = 'test';
+  final test = 'test';
 
   // expect_lint: avoid_hardcoded_strings
   print('Hello world: $test ${example.calculate()}!');
