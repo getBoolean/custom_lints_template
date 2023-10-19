d7477754272a8ffc31ca9696886e796f96c0f8e3
getBoolean <danielwnewville@gmail.com>
2023-10-19 01:19:25 -0500

    Temp remove expect_lint

--- example/random_clone/bin/random_clone.dart
+++ example/random_clone/bin/random_clone.dart
@@ -1,9 +1,7 @@
 import 'package:random_clone/random_clone.dart' as random;
 
 void main(List<String> arguments) {
-  // expect_lint: avoid_hardcoded_strings
   final test = 'test';
 
-  // expect_lint: avoid_hardcoded_strings
   print('Hello world: $test ${random.calculate()}!');
 }
